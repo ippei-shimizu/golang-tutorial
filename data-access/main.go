aa0065489d79dd1b47bbc21fb9289fb1aee8a439
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2025-01-26 22:28:19 +0900

    finished : Tutorial: Accessing a relational database

---
+++ data-access/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func main() {
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "mysql_container:3306",
+		DBName: "recordings",
+	}
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected!")
+}
+
+type Album struct {
+	ID int64
+	Title string
+	Artist string
+	Price float32
+}
+
+func albumsByArtist(name string) ([]Album, error) {
+	var albums []Album
+
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, err
+		}
+		albums = append(albums, alb)
+	}
+	return albums, nil
+}
+
+func albumByID(id int64) (Album, error) {
+	var alb Album
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		return alb, err
+	}
+	return alb, nil
+}
+
+func addAlbum(a Album) (int64, error) {
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+	if err != nil {
+		return 0, err
+	}
+	return result.LastInsertId()
+}
